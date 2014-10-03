1dff9ac4fd994039da3dbdeb5b43997e1cec2190
Radek Slupik <rightfold@gmail.com>
2014-10-03 23:24:13 +0200

    Implement file stream writing

--- include/baka/io/file_stream.hpp
+++ include/baka/io/file_stream.hpp
@@ -8,22 +8,68 @@ namespace baka {
     namespace io {
         enum class operation { read, write, seek };
 
+        namespace detail {
+            template<typename Self>
+            class file_stream_reader {
+            public:
+                char* read(char* begin, char* end) {
+                    auto& fd = static_cast<Self*>(this)->fd;
+                    auto n = ::read(fd.native_handle(), begin, end - begin);
+                    if (n == -1) {
+                        throw "fuck";
+                    }
+                    return begin + n;
+                }
+            };
+
+            template<typename Self>
+            class file_stream_writer {
+            public:
+                char const* write(char const* begin, char const* end) {
+                    auto& fd = static_cast<Self*>(this)->fd;
+                    auto n = ::write(fd.native_handle(), begin, end - begin);
+                    if (n == -1) {
+                        throw "fuck";
+                    }
+                    return begin + n;
+                }
+            };
+
+            template<typename Self>
+            class file_stream_seeker {
+                // TODO
+            };
+
+            template<operation op, typename... Args>
+            struct mixin_for_operation;
+
+            template<typename... Args>
+            struct mixin_for_operation<operation::read, Args...> {
+                using type = file_stream_reader<Args...>;
+            };
+
+            template<typename... Args>
+            struct mixin_for_operation<operation::write, Args...> {
+                using type = file_stream_writer<Args...>;
+            };
+
+            template<typename... Args>
+            struct mixin_for_operation<operation::seek, Args...> {
+                using type = file_stream_seeker<Args...>;
+            };
+        }
+
         template<typename FD, operation... supported_operations>
-        class file_stream {
+        class file_stream : public detail::mixin_for_operation<supported_operations, file_stream<FD, supported_operations...>>::type... {
         public:
             explicit file_stream(FD fd_) : fd(std::move(fd_)) { }
 
-            typename std::enable_if<contains_value<operation, operation::read, supported_operations...>::value, char*>::type
-            read(char* begin, char* end) {
-                auto n = ::read(fd.native_handle(), begin, end - begin);
-                if (n == -1) {
-                    throw "fuck";
-                }
-                return begin + n;
-            }
-
         private:
             FD fd;
+
+            friend class detail::file_stream_reader<file_stream>;
+            friend class detail::file_stream_writer<file_stream>;
+            friend class detail::file_stream_seeker<file_stream>;
         };
     }
 }
--- test/io/file_stream.cpp
+++ test/io/file_stream.cpp
@@ -4,17 +4,20 @@
 #include <cstring>
 #include <utility>
 
-TEST_CASE("read reads from the file stream", "[io::file_stream]") {
+TEST_CASE("read reads from the file stream and write writes to it", "[io::file_stream]") {
     int fds[2];
     pipe(fds);
     baka::io::unique_fd read_end(fds[0]);
     baka::io::unique_fd write_end(fds[1]);
 
-    char data[] = "the great baka";
-    write(write_end.native_handle(), data, sizeof(data));
+    baka::io::file_stream<decltype(write_end), baka::io::operation::write> wstream(std::move(write_end));
+    baka::io::file_stream<decltype(read_end), baka::io::operation::read> rstream(std::move(read_end));
 
-    baka::io::file_stream<decltype(read_end), baka::io::operation::read> stream(std::move(read_end));
+    char data[] = "the great baka";
+    wstream.write(data, data + sizeof(data));
+
     std::vector<char> result(sizeof(data));
-    stream.read(result.data(), result.data() + result.size());
+    rstream.read(result.data(), result.data() + result.size());
+
     REQUIRE(std::strcmp(data, result.data()) == 0);
 }
